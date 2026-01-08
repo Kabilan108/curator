7e57dd6c5c7a5a31abda8e5c6043f92b51fbc9fc
Tony Okeke <tonykabilanokeke@gmail.com>
2026-01-07 21:39:56 -0500

    remove vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import { ComponentExample } from "@/components/component-example";
-import { Analytics } from "@vercel/analytics/next";
 
 export function App() {
-  return (
-    <>
-      <ComponentExample />
-      <Analytics />
-    </>
-  );
+  return <ComponentExample />;
 }
 
 export default App;
